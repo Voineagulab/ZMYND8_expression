353bb669c8fb3e7be0df4e311458cebc7202a767
Urwah Nawaz <30397183+unawaz1996@users.noreply.github.com>
2021-01-22 12:21:40 +1100

    Update 02_time_expression.R

--- 02_time_expression.R
+++ 02_time_expression.R
@@ -23,36 +23,16 @@ suppressPackageStartupMessages({
 path.data <- path.expand("BrainSpan/Kang")
 
 #### upload all data
+ZMYND8_data <- read.csv("ZMYND8_metadata.csv", header = TRUE, row.names = 1)
 counts_matrix <- read.csv(file.path(path.data, "expression_matrix.csv"), header= FALSE, row.names= 1)
-columns_metadata <- read.csv("updated_metadata.csv", header = TRUE, row.names = 2)
 rows_metadata <- read.csv(file.path(path.data, "rows_metadata.csv"))
 
 
+#### Neocortex expression 
 
-### matrix 
-
-colnames(counts_matrix) <- columns_metadata$name_for_matrix
-rownames(counts_matrix) <- rows_metadata$ensembl_gene_id
-
-
-
-#### NCX expression 
-
-### subset columnmeta tot contain only NCX samples 
-
-NCX_samples <- columns_metadata %>% 
-  dplyr::filter(regions == "NCX") 
-
-NCX_matrix <- counts_matrix %>% 
-  dplyr::select(NCX_samples$name_for_matrix)
-
-
-NCX <- NCX_matrix["ENSG00000101040",] %>%
-  as.data.frame() %>% 
-  rownames_to_column("geneID") %>%
-  melt() %>% 
-  cbind(NCX_samples) %>%
-  ggplot(aes(stage, value))  + 
+NCX <- ZMYND8_data %>%
+  dplyr::filter(regions == "NCX") %>%
+  ggplot(aes(stage, ZMYND8_expression))  + 
   geom_boxplot(aes(fill=stage), alpha=0.65)  + 
   geom_jitter(size=2.5) +
   scale_color_viridis(discrete = TRUE, option = "magma")+ theme_bw() +
@@ -77,44 +57,33 @@ NCX <- NCX_matrix["ENSG00000101040",] %>%
                                                    "s12" = "30 - 39 yrs", 
                                                    "s13" = ">=40 yrs"))
 
-
 ### Other regions 
-
-OtherRegions_samples <-  columns_metadata %>% 
-  dplyr::filter(regions == "Other_regions")
-
-OtherRegions_matrix <- counts_matrix %>%
-  dplyr::select(OtherRegions_samples$df_name)
-
-OR <- OtherRegions_matrix["ENSG00000101040",] %>%
-  as.data.frame() %>% 
-  rownames_to_column("geneID") %>%
-  melt() %>% 
-  cbind(OtherRegions_samples) %>%  
-  ggplot(aes(stage, value))  + 
+OR <- ZMYND8_data %>%
+  dplyr::filter(regions == "Other_regions") %>%
+  ggplot(aes(stage, ZMYND8_expression))  + 
   geom_boxplot(aes(fill=stage), alpha=0.65)  + 
   geom_jitter(size=2.5) +
-  scale_color_viridis(discrete = TRUE, option = "magma")+
-  scale_fill_viridis(discrete = TRUE, direction = 1)  + theme_bw() +
-  ylab("RPKM") + xlab("Stage") +
+  scale_color_viridis(discrete = TRUE, option = "magma")+ theme_bw() +
+  scale_fill_viridis(discrete = TRUE, direction = 1)  +
+  xlab("Stage") + ylab("RPKM") +
   theme(legend.position = "none", 
         axis.text=element_text(size=11, face="bold", color="black"), 
         axis.title = element_text(siz=12, face="bold", color="black"), 
         axis.text.x = element_text(angle=90)) +
   ggtitle("Other regions") + scale_x_discrete(labels=c("s02a" = "8 - 9pcw", 
-                                                       "s02b" = "10 - 12pcw", 
-                                                       "s03a" = "13 - 15pcw", 
-                                                       "s03b" = "16 - 18pcw",
-                                                       "s04" =  "19 - 24pcw", 
-                                                       "s05" = "25 - 38pcw", 
-                                                       "s06" = "Birth - 5 mos", 
-                                                       "s07" = "6 - 18 mos", 
-                                                       "s08" = "19 mos - 5yrs", 
-                                                       "s09" = "6 - 11 yrs",
-                                                       "s10" = "12 - 19 yrs",
-                                                       "s11" = "20 - 29 yrs", 
-                                                       "s12" = "30 - 39 yrs", 
-                                                       "s13" = ">=40 yrs"))
+                                                   "s02b" = "10 - 12pcw", 
+                                                   "s03a" = "13 - 15pcw", 
+                                                   "s03b" = "16 - 18pcw",
+                                                   "s04" =  "19 - 24pcw", 
+                                                   "s05" = "25 - 38pcw", 
+                                                   "s06" = "Birth - 5 mos", 
+                                                   "s07" = "6 - 18 mos", 
+                                                   "s08" = "19 mos - 5yrs", 
+                                                   "s09" = "6 - 11 yrs",
+                                                   "s10" = "12 - 19 yrs",
+                                                   "s11" = "20 - 29 yrs", 
+                                                   "s12" = "30 - 39 yrs", 
+                                                   "s13" = ">=40 yrs"))
 
 
 
@@ -132,6 +101,12 @@ dev.off()
 
 
 ##### neocortex 
+NCX_samples <- columns_metadata %>% 
+  dplyr::filter(regions == "NCX") 
+
+NCX_matrix <- counts_matrix %>% 
+  dplyr::select(NCX_samples$name_for_matrix)
+
 counts_ZMYND8_NCX <- NCX_matrix[rownames(NCX_matrix) %in% c("ENSG00000101040"),]
 
 NCX_pval <- apply(t(counts_ZMYND8_NCX),2, function(x) cor.test(x, NCX_samples$ages, method="s"))
@@ -139,6 +114,11 @@ developmental_stages_NCX <- do.call("rbind", NCX_pval)
 
 
 ##### Other regions 
+OtherRegions_samples <-  columns_metadata %>% 
+  dplyr::filter(regions == "Other_regions")
+OtherRegions_matrix <- counts_matrix %>%
+  dplyr::select(OtherRegions_samples$df_name)
+
 counts_ZMYND8_OR <- OtherRegions_matrix[rownames(OtherRegions_matrix) %in% c("ENSG00000101040"),]
 OR_pval <- apply(t(counts_ZMYND8_OR),2, function(x) cor.test(x, OtherRegions_samples$ages, method="s"))
 developmental_stages_OR <- do.call("rbind", OR_pval)
